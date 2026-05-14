1978eb278c2c5d479bfae0ee6b2c2f78e14cc58f
ShahriyarSaidi <sehriyarseidi@gmail.com>
2026-05-15 00:20:38 +0400

    saytda dəyişiklik etdim

--- js/main.js
+++ js/main.js
@@ -144,32 +144,6 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-document.addEventListener("DOMContentLoaded", function () {
-  const copyButton = document.querySelector(".password-copy-icon");
-  const passwordDiv = document.querySelector(".password");
-  const toast = document.querySelector(".copy-toast");
-
-  if (copyButton && passwordDiv) {
-    copyButton.addEventListener("click", async function () {
-      const fullText = passwordDiv.textContent.trim();
-      const password = fullText.replace("Sifre:", "").trim();
-
-      try {
-        await navigator.clipboard.writeText(password);
-
-        // Toast göstər
-        toast.classList.add("show");
-
-        setTimeout(() => {
-          toast.classList.remove("show");
-        }, 2000);
-
-      } catch (err) {
-        console.error("Kopyalama alınmadı:", err);
-      }
-    });
-  }
-});
 
 
 
